8c2fec12fc7d2dc7b457ed7a79835f481d42e6a3
Emanuel Ben Hamo <embenh@gmail.com>
2020-03-23 01:05:54 +0200

    refactor script

--- process.js
+++ process.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const axios = require('axios');
-const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
 const BASE_URL = 'https://api.unsplash.com';
 const ACCESS_KEY = 'nbR00CogATeI3qs7vHcOpuJAahhzzRWxYTTB0Pd4bZM';
@@ -12,23 +11,10 @@ const USER_NAME = "fiversprint";
 const MEN_CSV_FILE = './data/menImgUrl.csv';
 const WOMEN_CSV_FILE = './data/womenImgUrl.csv';
 
-const menCsvWriter = createCsvWriter({
-    path: MEN_CSV_FILE,
-    header: [
-        { id: 'imgUrl', title: 'img_urls_list' }
-    ]
-});
-const womenCsvWriter = createCsvWriter({
-    path: WOMEN_CSV_FILE,
-    header: [
-        { id: 'imgUrl' }
-    ]
-});
-
 async function getAllCollections() {
     let collections = [];
 
-    for (var i = 1; i <= 2; i++) {
+    for (var i = 1; i <= 3; i++) {
         let result = await axios.get(`${BASE_URL}/users/${USER_NAME}/collections/?client_id=${ACCESS_KEY}&&page=${i}`);
         collections.push(...result.data);
     }
@@ -36,6 +22,14 @@ async function getAllCollections() {
     console.log(`total collections count is ${collections.length}`);
 
     return collections;
+}
+
+function filterCollectionsIdsByGender(collections, gender) {
+    const genderCollectionArr = filterCollectionsByGender(collections, gender)
+        .map(womenCollection => womenCollection.id);
+    console.log(`total ${gender} collections is ${genderCollectionArr.length}`);
+
+    return genderCollectionArr;
 }
 
 function filterCollectionsByGender(collections, gender) {
@@ -48,6 +42,19 @@ function filterCollectionsByGender(collections, gender) {
     }
 
     return collections.filter(collection => collection.title[collection.title.length - 1] === filterChararcter);
+}
+
+async function getResArr(collectionIdsArr) {
+    let prmsArr = [];
+    collectionIdsArr.forEach(photoCollectionId => {
+        let photosUrlsPrms = getCollectionPhotoUrls(photoCollectionId);
+        prmsArr.push(photosUrlsPrms);
+    });
+
+    let resArr = await Promise.all(prmsArr);
+    resArr = resArr.map(urlArr => JSON.stringify(urlArr));
+
+    return resArr;
 }
 
 async function getCollectionPhotoUrls(collectionId) {
@@ -70,33 +77,29 @@ function getPhotosUrl(imgDataArr) {
     return imgDataArr.map(imgData => imgData.urls.regular);
 }
 
+function saveToFile(file, arr) {
+    fs.writeFileSync(file, '');
+    arr.forEach(value => {
+        fs.appendFileSync(file, `"${value.replace(/"/g, "'")}"\n`);
+    });
+}
+
 async function run() {
     // get all of user photos collections
     const collections = await getAllCollections();
 
-    const womenCollectionsIds = filterCollectionsByGender(collections, "women")
-        .map(womenCollection => womenCollection.id);
-    console.log(`total women collections is ${womenCollectionsIds.length}`);
-
-    const menCollectionsIds = filterCollectionsByGender(collections, "men")
-        .map(menCollection => menCollection.id);
-    console.log(`total men collections is ${menCollectionsIds.length}`);
-
-    let prmsArr = [];
-    womenCollectionsIds.forEach(photoCollectionId => {
-        let photosUrlsPrms = getCollectionPhotoUrls(photoCollectionId);
-        prmsArr.push(photosUrlsPrms);
-    });
-
-    let resArr = await Promise.all(prmsArr);
-    resArr = resArr.map(urlArr => JSON.stringify(urlArr));
-
+    // filter collections by gender
+    // women
+    const womenCollectionsIds = filterCollectionsIdsByGender(collections, "women");
+    let resArr = await getResArr(womenCollectionsIds);
+    saveToFile(WOMEN_CSV_FILE, resArr);
     console.log('done writing to women file', resArr);
 
-    fs.writeFileSync(WOMEN_CSV_FILE, '');
-    resArr.forEach(value => {
-        fs.appendFileSync(WOMEN_CSV_FILE, `"${value.replace(/"/g, "'")}"\n`);
-    });
+    // men
+    const menCollectionsIds = filterCollectionsIdsByGender(collections, "men");
+    let resArr = await getResArr(menCollectionsIds);
+    saveToFile(MEN_CSV_FILE, resArr);
+    console.log('done writing to men file', resArr);
 }
 
 run();
