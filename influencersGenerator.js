eda8c4e0cc168f7004aa369cc4de05aa4e94e95b
MatanBenHamo <62705987+MatanBenHamo@users.noreply.github.com>
2020-03-26 16:28:49 +0200

    added profile photo

--- influencersGenerator.js
+++ influencersGenerator.js
@@ -1,56 +1,72 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const menPhotos = require('./data/menImgUrl.json');
-const womenPhotos = require('./data/womenImgUrl.json');
-const influencers = require('./data/influencers.json');
+const menPhotos = require("./data/menImgUrl.json");
+const womenPhotos = require("./data/womenImgUrl.json");
+const influencers = require("./data/influencers.json");
 
-const FILE_TO_SAVE = './data/influencersWithPhotos.json';
+const FILE_TO_SAVE = "./data/influencersWithPhotos.json";
 
 function getInfluencersWithPhotos(influencers) {
-    let influencersWithPhotos = [];
-    const womenInfluencers = influencers.filter(influencer => influencer.gender === "Female");
-    const menInfluencers = influencers.filter(influencer => influencer.gender === "Male");
+  let influencersWithPhotos = [];
+  const womenInfluencers = influencers.filter(
+    influencer => influencer.gender === "Female"
+  );
+  const menInfluencers = influencers.filter(
+    influencer => influencer.gender === "Male"
+  );
 
-    const womenInfluencersWithPhotos = getInfluencersWithPhotosByGender(womenInfluencers, womenPhotos);
-    const menInfluencersWithPhotos = getInfluencersWithPhotosByGender(menInfluencers, menPhotos);
+  const womenInfluencersWithPhotos = getInfluencersWithPhotosByGender(
+    womenInfluencers,
+    womenPhotos
+  );
+  const menInfluencersWithPhotos = getInfluencersWithPhotosByGender(
+    menInfluencers,
+    menPhotos
+  );
 
-    influencersWithPhotos.push(...womenInfluencersWithPhotos);
-    console.log(`-- created ${womenInfluencersWithPhotos.length} women influencers with photos`);
-    influencersWithPhotos.push(...menInfluencersWithPhotos);
-    console.log(`-- created ${menInfluencersWithPhotos.length} men influencers with photos`);
+  influencersWithPhotos.push(...womenInfluencersWithPhotos);
+  console.log(
+    `-- created ${womenInfluencersWithPhotos.length} women influencers with photos`
+  );
+  influencersWithPhotos.push(...menInfluencersWithPhotos);
+  console.log(
+    `-- created ${menInfluencersWithPhotos.length} men influencers with photos`
+  );
 
-    return influencersWithPhotos;
+  return influencersWithPhotos;
 }
 
 function getInfluencersWithPhotosByGender(influencers, photos) {
-    let influencersWithPhotos = [];
-    let numOfPhotos = photos.length;
-    let numOfInfluencers = influencers.length;
-    let maxNumOfPersons = Math.min(numOfPhotos, numOfInfluencers);
+  let influencersWithPhotos = [];
+  let numOfPhotos = photos.length;
+  let numOfInfluencers = influencers.length;
+  let maxNumOfPersons = Math.min(numOfPhotos, numOfInfluencers);
 
-    for (let i = 0; i < maxNumOfPersons; i++) {
-        influencers[i].photos = photos[i].urls;
-        influencersWithPhotos.push(influencers[i]);
-    }
+  for (let i = 0; i < maxNumOfPersons; i++) {
+    influencers[i].photos = photos[i].urls;
+    let randomIndex = Math.floor(Math.random() * photos[i].urls.length);
+    influencers[i].profilePhoto = photos[i].urls[randomIndex];
+    influencersWithPhotos.push(influencers[i]);
+  }
 
-    return influencersWithPhotos;
+  return influencersWithPhotos;
 }
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
 function run() {
-    console.log('--- influencers generator started ---');
+  console.log("--- influencers generator started ---");
 
-    let influencersWithPhotos = getInfluencersWithPhotos(influencers);
-    shuffle(influencersWithPhotos);
-    fs.writeFileSync(FILE_TO_SAVE, JSON.stringify(influencersWithPhotos));
+  let influencersWithPhotos = getInfluencersWithPhotos(influencers);
+  shuffle(influencersWithPhotos);
+  fs.writeFileSync(FILE_TO_SAVE, JSON.stringify(influencersWithPhotos));
 
-    console.log('--- influencers generator finished ---');
+  console.log("--- influencers generator finished ---");
 }
 
 run();
